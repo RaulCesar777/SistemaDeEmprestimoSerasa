b2e537a2837e6de58af693fc76a94780f0e2f154
raulz <raulz@DESKTOP-Q17PTAJ>
2021-06-10 03:40:37 -0300

    adicionando laço for

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -12,13 +12,55 @@ public class Program {
 		
 		Client client = new Client();
 		
-		client.setFullName("João Da Silva Galderio");
-		client.setCPF("123.456.78-9 ");;
-		client.setRG("0.123.456-5");
-		client.setBirthDate("01/02/1989");
-		client.setSalary1(5000);
-		client.setSalary2(7000.00);
-		client.setSalary3(10000.00);
+		String FullName;
+		String CPF;
+		String RG;
+		String birthDate;
+		double salary;
+		
+		
+		System.out.println("Nome do cliente");
+		FullName= sc.nextLine();		
+		client.setFullName(FullName);
+		
+		System.out.println("CPF do cliente");
+		CPF=sc.nextLine();
+		client.setCPF(CPF);
+		
+		System.out.println("RG do cliente");
+		RG=sc.nextLine();
+		client.setRG(RG);
+		
+		System.out.println("Data de Nascimento do cliente");
+		birthDate=sc.nextLine();
+		client.setBirthDate(birthDate);
+		
+		System.out.println("Prescisamos dos tres ultimos vencimentos");
+		
+		for(int i=1; i<=3; i++) {
+			
+			System.out.println("folha de pagamento Nº \n"+ i);
+			salary = sc.nextDouble();
+			switch (i) {
+			  case 1:
+				  client.setSalary1(salary);
+			    break;
+			  case 2:
+				  client.setSalary2(salary);
+			    break;
+			  case 3:
+				  client.setSalary3(salary);
+			    break;
+			  default:
+			    System.out.println("sem opção");
+			}
+			
+			
+		}
+		
+		
+		
+		
 		
 		
 		System.out.println(client.toString());
